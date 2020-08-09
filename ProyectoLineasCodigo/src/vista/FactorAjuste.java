7d58600a78f1900b5b7cf12bd043d6dd42593d05
Alexandra López <romerolopez2306@gmail.com>
2020-08-08 23:00:07 -0500

    Corrección Resolución Pantalla
    Para pantallas pequeñas

--- ProyectoLineasCodigo/src/vista/FactorAjuste.java
+++ ProyectoLineasCodigo/src/vista/FactorAjuste.java
@@ -24,7 +24,6 @@ public class FactorAjuste extends javax.swing.JFrame {
     public FactorAjuste() {
         initComponents();
         this.setLocationRelativeTo(null);
-
         ArrayList<Integer> atributos = VentanaPrincipal.atributos;
         if (atributos.size() > 0) {
 
@@ -360,10 +359,10 @@ public class FactorAjuste extends javax.swing.JFrame {
                                 .addComponent(cbx_base_datos, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                 .addComponent(cbx_fiabilidad, javax.swing.GroupLayout.PREFERRED_SIZE, 99, javax.swing.GroupLayout.PREFERRED_SIZE)))))
                 .addGap(74, 74, 74))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(162, 162, 162)
                 .addComponent(btn_aceptar, javax.swing.GroupLayout.PREFERRED_SIZE, 156, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(170, 170, 170))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -443,9 +442,9 @@ public class FactorAjuste extends javax.swing.JFrame {
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(cbx_rest_tiempo_desarrollo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel15))
-                .addGap(66, 66, 66)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(btn_aceptar, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(69, Short.MAX_VALUE))
+                .addContainerGap(23, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
